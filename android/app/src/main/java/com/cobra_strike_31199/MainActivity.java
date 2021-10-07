c7cee80d0c7a8acff7bfe4a4edbd29e1aa4276f7
Crowdbotics <team@crowdbotics.com>
2021-10-07 12:58:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cobra_strike_31199/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cobra_strike_31199;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cobra_strike_31199";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cobra_strike_31199",
+  url: "https://cobra_strike_31199.botics.co",
+  api: "https://cobra_strike_31199.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
